120c9cba9187389420a61a74dcb3cb979c8d1532
a23lisjo <a23lisjo@student.his.se>
2024-05-20 12:03:48 +0200

    Hämtning av data samt tillagd recycler view.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a23lisjo";
+
+    ArrayList<owl> owls= new ArrayList<>();
+    Gson gson = new Gson();
+    String json = gson.toJson(owls);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        new JsonTask (this).execute(JSON_URL);
+
+
+
+        /*ArrayList<mountain> mountains = new ArrayList<>();
+        mountains.add( new mountain("k2"));*/
+
+
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Type type = new TypeToken<ArrayList<owl>>() {}.getType();
+        ArrayList<owl> owls = gson.fromJson(json, type);
+
+        RecyclerView recycler_view = findViewById(R.id.widgetrecycler);
+
+        recyclerviewadapter adapter = new recyclerviewadapter (this, owls);
+        recycler_view.setAdapter(adapter);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+
     }
 
 }
+
