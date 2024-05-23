f4f454b4f18e4cd9825426ea159d55df69ae70cc
a23lisjo <a23lisjo@student.his.se>
2024-05-23 17:08:48 +0200

    Tillagd webView

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        myWebView = findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
 }
